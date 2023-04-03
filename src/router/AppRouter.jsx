a6d1e69bede275e25e890e95e5e4e81b0b200a46
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-04-03 23:11:50 +0300

    detail css added

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,13 @@
+import React from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import Dashboard from "../pages/Dashboard"
-import NavBar from "../components/NavBar"
-import Footer from "../components/Footer"
-import NewBlog from "../pages/NewBlog"
-import About from "../pages/About"
+import Dashboard from "../pages/Dashboard";
+import NavBar from "../components/NavBar";
+import NewBlog from "../pages/NewBlog";
+import About from "../pages/About";
+
+import Footer from "../components/Footer";
+import Detail from "../pages/Detail";
+
 const AppRouter = () => {
   return (
     <Router>
@@ -12,7 +16,7 @@ const AppRouter = () => {
             <Route path="/" element={<Dashboard />} />
             <Route path="/about" element={<About/>} />
             <Route path="/new-blog" element={<NewBlog/>} />
-
+            <Route path="/detail/:id" element={<Detail/>} />
         </Routes>
         <Footer/>
     </Router>
