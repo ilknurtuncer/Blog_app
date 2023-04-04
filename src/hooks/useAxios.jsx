31155bc31555a8e6b3f3c559b0052963886b0ebc
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-04-05 00:03:12 +0300

    useaxios cerated

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,10 @@
+import axios from "axios"
+
+
+
+const useAxios = ()=>{}
+const token = "Token 16e1828d99f7d1d81e481902da6e70a004284c62"
+const axiosPublic = axios.create({
+    baseURL:"http://32304.fullstack.clarusway.com/"
+})
+
