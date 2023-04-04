a7d93c15772717a5e0975a41bdf57e57a13dc549
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-04-05 00:05:05 +0300

    axiosWithToken

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,9 +2,16 @@ import axios from "axios"
 
 
 
-const useAxios = ()=>{}
+const useAxios = ()=>{
 const token = "Token 16e1828d99f7d1d81e481902da6e70a004284c62"
 const axiosPublic = axios.create({
     baseURL:"http://32304.fullstack.clarusway.com/"
 })
 
+const axiosWithToken = axios.create({
+    baseURL:"http://34616.fullstack.clarusway.com/",
+    headers:{Authorization: `Token ${token}`}
+})
+return {axiosPublic, axiosWithToken}
+}
+export default useAxios
